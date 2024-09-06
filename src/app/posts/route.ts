01d7b7dcb5877013a079b656293e65c9894d34cc
kyyril <khairilrahman55@gmail.com>
2024-09-06 23:09:20 +0700

    next request & response

--- src/app/posts/route.ts
+++ src/app/posts/route.ts
@@ -1,9 +1,10 @@
+import { NextResponse, NextRequest } from "next/server";
 import { Data } from "./data";
 export async function GET() {
-  return Response.json(Data);
+  return NextResponse.json(Data);
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const post = await request.json();
   const newPost = {
     id: Data.length + 1,
