f9a23a461a9a68c1e10984e3f27f4565afbada4d
kyyril <khairilrahman55@gmail.com>
2024-09-07 16:55:38 +0700

    cookie

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,9 +1,12 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const listHeader = headers();
   console.log(listHeader.get("Authorization"));
+  console.log(request.cookies.get("token"));
+  console.log(cookies().get("theme"));
+  console.log(cookies().set("theme", "dark"));
   return new Response("<h1>Halo Dunia</h1>", {
     headers: {
       "Content-Type": "text/html",
